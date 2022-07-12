89cfb12d4c7bd5e80e8d8357ffe5177746c74e13
BogdanIvanilov <106593851+BogdanIvanilov@users.noreply.github.com>
2022-07-13 01:31:59 +0300

    fix bug

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function reverse (n) {
 	let a = String(n);
 	let b = Array.from(a);
 	if(b[0] == '-'){
-		b.shift()
+		b.shift();
 	}
 	let c = b.reverse();
 	let output = Number(c.join(''));
