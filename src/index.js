4334313eb7a1d11a2e02ed3a0e4eb777298e6493
BogdanIvanilov <awesomest@mzfk.com>
2022-07-13 01:21:04 +0300

    made the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+	let a = String(n);
+	let b = Array.from(a);
+	if(b[0] == '-'){
+		b.shift()
+	}
+	let c = b.reverse();
+	let output = Number(c.join(''));
+	return output;
 }
